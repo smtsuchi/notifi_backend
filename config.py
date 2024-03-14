45e92f2bfbf9356d0376cff0fe5b04b595d29413
smtsuchi <shoha.tsuchida@gmail.com>
2024-03-14 11:20:11 -0400

    push to heroku

--- config.py
+++ config.py
@@ -8,9 +8,11 @@ class Config():
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_COOKIE_SECURE = True
-    JWT_COOKIE_CSRF_PROTECT  = True
+    JWT_COOKIE_CSRF_PROTECT  = False
     JWT_TOKEN_LOCATION = ["cookies"]
     JWT_SECRET_KEY = os.environ.get("SECRET_KEY")
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
     JWT_ERROR_MESSAGE_KEY = "message"
     CORS_SUPPORTS_CREDENTIALS=True
+    JWT_ACCESS_COOKIE_NAME = "__session"
+    JWT_JSON_KEY = "__session"
